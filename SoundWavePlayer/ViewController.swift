c118518bb942514f2a1fb306c909fc4078a771aa
Spiridonov Maxim <taijased@gmail.com>
2020-06-06 11:41:30 +0300

    Initial Commit

---
+++ SoundWavePlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SoundWavePlayer
+//
+//  Created by Максим Спиридонов on 06.06.2020.
+//  Copyright © 2020 Максим Спиридонов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
